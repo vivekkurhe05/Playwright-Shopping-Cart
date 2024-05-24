aa627a1dae5067db37ea5707e52870a2f6f933d9
Vivek_Ennotech <vivek_ennotech@192.168.1.10>
2024-05-24 22:44:08 +0530

    e2e test

--- page-objects/product.page.js
+++ page-objects/product.page.js
@@ -1,10 +1,31 @@
+import { expect } from "@playwright/test"
+
 export class ProductPage {
 
     constructor(page) {
         this.page = page
+        this.addButtons = page.locator('[data-qa="product-button"]')
+        this.basketCounter = page.locator('[data-qa="header-basket-count"]')
     }
 
     visit = async () => {
         await this.page.goto("/")
+    }
+
+    getBasketCount = async () => {
+        await this.basketCounter.waitFor()
+        const text = await this.basketCounter.innerText()
+        return parseInt(text)
+    }
+
+    addProductToBasket = async (index) => {
+        const specificAddButton = this.addButtons.nth(index)
+        await specificAddButton.waitFor()
+        await expect(specificAddButton).toHaveText("Add to Basket")
+        const basketCountBeforeAdding = await this.getBasketCount()
+        await specificAddButton.click()
+        await expect(specificAddButton).toHaveText("Remove from Basket")
+        const basketCountAfterAdding = await this.getBasketCount()
+        expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding)
     }
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -3,8 +3,11 @@ import { ProductPage } from "../page-objects/product.page"
 
 test.only("New user full e2e test journey", async ({ page }) => {
 
-    // ProductPage.visit()
     const productsPage = new ProductPage(page)
     await productsPage.visit()
+    await productsPage.addProductToBasket(0)
+    await productsPage.addProductToBasket(1)
+    await productsPage.addProductToBasket(2)
+
     // await page.pause()
 })
